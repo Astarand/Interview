7ce2c6f10406415ad5879aef526bb2397610bf11
Rittik <Rittik.SK@hotmail.com>
2023-07-01 18:36:39 +0530

    rectify

--- js/custom.js
+++ js/custom.js
@@ -73,6 +73,14 @@ $(document).ready(function() {
         }
     });
 });
+let btn= document.querySelector('.bt_btn');
+btn.addEventListener('click',function(){
 
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
 
 
